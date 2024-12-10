d51e27db04c4a2f2262b6dcc49a509b4bab8fe16
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-12-09 21:10:11 -0500

    feat: :art: Verificacion de las funcioenes y funciona procedemoas a acrear las otoras dos funcionies y luego a dalre una interfas

--- Modulos/Crud.py
+++ Modulos/Crud.py
@@ -1,4 +1,5 @@
 import Modulos.BaseDatosJson as BaseDatos
+import Modulos.Menu as Menu
 import os
 
 def agregar_tarea():
@@ -10,6 +11,8 @@ def agregar_tarea():
     
     BaseDatos.Guarda(tareas)
     print("Tarea agregada exitosamente.")
+    os.system("pause")
+    Menu.menu()
 
 def listar_tareas():
     tareas=BaseDatos.Carga()
@@ -18,24 +21,30 @@ def listar_tareas():
     else:
         for tarea_id, tarea in tareas.items():
             print(f"Tarea {tarea_id}: {tarea['titulo']} - {tarea['descripcion']} ({tarea['estado']})")
+    os.system("pause")
+    Menu.menu()
             
 
 def marcar_como_completada():
     tareas=BaseDatos.Carga()
-    tarea_id = int(input("Ingrese el ID de la tarea a marcar como completada: "))
+    tarea_id = str(input("Ingrese el ID de la tarea a marcar como completada: "))
     if tarea_id in tareas:
         tareas[tarea_id]['estado'] = 'completado'
         print("Tarea marcada como completada.")
         BaseDatos.Guarda(tareas)
     else:
         print("Tarea no encontrada.")
+    os.system("pause")
+    Menu.menu()
 
 def eliminar_tarea():
     tareas=BaseDatos.Carga()
-    tarea_id = int(input("Ingrese el ID de la tarea a eliminar: "))
+    tarea_id = str(input("Ingrese el ID de la tarea a eliminar: "))
     if tarea_id in tareas:
         del tareas[tarea_id]
         print("Tarea eliminada.")
         BaseDatos.Guarda(tareas)
     else:
         print("Tarea no encontrada.")
+    os.system("pause")
+    Menu.menu()
--- Modulos/Menu.py
+++ Modulos/Menu.py
@@ -2,38 +2,35 @@ import Modulos.Crud as Crud
 import sys
 import os
 
-
-title="""
-        Menú de Gestión de Tareas
-
-"""
-ListaMenu=[
-        "Agregar tarea",
-        "Listar tareas",
-        "Marcar como completada",
-        "Eliminar tarea",
-        "Exportar tareas",
-        "Importar tareas",
-        "Salir"
-]
-
-
 def menu():
-    os.system("cls")
-    print(title)
-    try:
-        [print(f"{i+1}. {item}") for i, item in enumerate(ListaMenu)]
-        Op=int(input("Seleccione: >>_"))
-        match Op:
-            case 1: Crud.agregar_tarea()
-            case 2: Crud.listar_tareas()
-            case 3: Crud.marcar_como_completada()
-            case 4: Crud.eliminar_tarea()
-            case 5:
-                pass
-            case 6:
-                pass
-            case 7: sys.exit() 
-        
-    except ValueError:
-            print("El valor ingresado no es valido ingrese una opcion valida")
+    title="""
+            Menú de Gestión de Tareas
+    """
+    ListaMenu=[
+            "Agregar tarea",
+            "Listar tareas",
+            "Marcar como completada",
+            "Eliminar tarea",
+            "Exportar tareas",
+            "Importar tareas",
+            "Salir"
+    ]
+    while True:
+        os.system("cls")
+        print(title)
+        try:
+            [print(f"{i+1}. {item}") for i, item in enumerate(ListaMenu)]
+            Op=int(input("Seleccione: >>_"))
+            match Op:
+                case 1: Crud.agregar_tarea()
+                case 2: Crud.listar_tareas()
+                case 3: Crud.marcar_como_completada()
+                case 4: Crud.eliminar_tarea()
+                case 5:
+                    pass
+                case 6:
+                    pass
+                case 7: sys.exit() 
+            
+        except ValueError:
+                print("El valor ingresado no es valido ingrese una opcion valida")
